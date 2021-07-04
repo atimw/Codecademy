a046486e1fb54a0f3d83c267be5de01383111416
Tim Windhorst <atimw36@gmail.com>
2021-07-03 22:41:01 -0400

    Finished number guesser with extra function separation and input checker

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -8,12 +8,30 @@ function generateTarget(){
     return Math.floor(Math.random()*9);
 }
 
+function getAbsoluteDistance(num1,num2){
+    return Math.abs(num1 - num2);
+}
+
+function checkInput(num1,num2){
+    if (num1 <= 9 && num1 >= 0 && num2 <= 9 && num2 >= 0){
+        return true;
+    }
+}
+
 /* compare guesses of user and computer */
 function compareGuesses(human,computer,target){
     /* convert human and computer guesses to absolute value of guess compared to target */
-    const userGuess = Math.abs(target - human);
-    const computerGuess = Math.abs(target - computer);
-    return userGuess <= computerGuess;
+    // if not before the first round
+    if (currentRoundNumber > 0){
+        //check user input
+        if (checkInput(human,computer)){
+            const userGuess = getAbsoluteDistance(target,human);
+            const computerGuess = getAbsoluteDistance(target,computer);
+            return userGuess <= computerGuess;
+        } else {
+            alert('Please enter a number between 0 and 9');
+        }
+}
 }
 
 /* update the winners score each round */
